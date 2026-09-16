9354ecaf3c4214e1c4e3e2f1276a0d946f9b0118
Matt Farrell <10377148+MattFaz@users.noreply.github.com>
2026-09-17 08:40:21 +0930

    Add support site FAQ link

--- Actuali/Actuali/Views/Settings/SupportView.swift
+++ Actuali/Actuali/Views/Settings/SupportView.swift
@@ -3,11 +3,14 @@ import SwiftUI
 private let discordURL = URL(string: "https://discord.gg/PDcJDPYpDG")!
 private let githubURL = URL(string: "https://github.com/MattFaz/actuali")!
 private let contactEmailURL = URL(string: "mailto:actuali@mfazz.com")!
+private let supportSiteURL = URL(string: "https://actuali.mfazz.com/support")!
 
 struct SupportView: View {
     var body: some View {
         Form {
             Section(String(localized: "Help & Links")) {
+                Link(String(localized: "Help & FAQ"), destination: supportSiteURL)
+                    .accessibilityIdentifier("support.website")
                 Link(String(localized: "Discord"), destination: discordURL)
                     .accessibilityIdentifier("support.discord")
                 Link(String(localized: "GitHub"), destination: githubURL)
